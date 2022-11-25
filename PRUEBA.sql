d13e92ba70dc468bc77c6a9086f1685e6e6bcbe9
PabloSanchez2002 <pablosanchez2002@gmail.com>
2022-11-25 13:00:56 +0100

    sd

---
+++ PRUEBA.sql
@@ -0,0 +1,13 @@
+400 películas británicas (o de
+colaboración británica) más actuales en base a la información almacenada en la Base de Datos
+relacional. Los documentos tendrán la siguiente estructura:
+    a. Título sin el año – cadena
+    b. Géneros – listado de géneros
+    c. Año – número
+    d. Valoraciones recibidas – número
+    e. Valoración media – número real
+    f. Directores – listado de directores
+    g. Actores participantes – listado de actores
+    h. Hasta 10 películas más actuales y relacionadas – listado de títulos, años y valoraciones
+    medias
+
